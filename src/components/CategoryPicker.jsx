8ffd591cd1f6206038a5c6540c65672ccbdfdc47
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-05-06 22:07:57 +0200

    Filter by category functionality added

---
+++ src/components/CategoryPicker.jsx
@@ -0,0 +1,21 @@
+const CategoryPicker = ({handleSelect}) => {
+    return (
+        <select onChange={(e) => handleSelect(e.target.value)}>
+            <option value="">Välj kategori...</option>
+            <option value="Djur och natur">Djur och natur</option>
+            <option value="E-sport och spel">E-sport och spel</option>
+            <option value="Fritid och hobby">Fritid och hobby</option>
+            <option value="Konst och design">Konst och design</option>
+            <option value="Kreativt skapande">Kreativt skapande</option>
+            <option value="Fri lek">Fri lek</option>
+            <option value="Lärande och språk">Lärande och språk</option>
+            <option value="Nöje">Nöje</option>
+            <option value="Quiz och frågesport">Quiz och frågesport</option>
+            <option value="Sport och idrott">Sport och idrott</option>
+            <option value="Utställning">Utställning</option>
+            <option value="Workshop">Workshop</option>
+        </select>
+    )
+}
+
+export default CategoryPicker
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import { FetchAllEventsByDate } from "./FetchAllEventsByDate";
 import EventCard from "./EventCard";
 import DatePicker from "./DatePicker";
+import CategoryPicker from "./CategoryPicker";
 
 export default function EventList() {
 
-	const [items, setItems] = useState(null);
+	const [allItems, setAllItems] = useState([]);
+	const [items, setItems] = useState([]);
+	const [category, setCategory] = useState("");
 	const [today, setToday] = useState(() => {
 		return new Date().toISOString().split("T")[0];
 	});
@@ -14,9 +17,25 @@ export default function EventList() {
 		setToday(pick);
 	}
 
+	const handleSelect = (pick) => {
+		setCategory(pick);
+	}
+
+	useEffect(() => {
+
+		if (!category) {
+			setItems(allItems); // visa allt om ingen kategori är vald
+			return;
+		}
+
+		const filteredItems = allItems.filter(item => item.categories.includes(category));
+		setItems(filteredItems);
+	}, [category, allItems]);
+
 	useEffect(() => {
 		const load = async () => {
 			const data = await FetchAllEventsByDate(today);
+			setAllItems(data);
 			setItems(data);
 		};
 
@@ -27,7 +46,10 @@ export default function EventList() {
 
 	return (
 		<>
-			<DatePicker handleChange={handleChange} />
+			<div id='search-bar'>
+				<DatePicker handleChange={handleChange} />
+				<CategoryPicker handleSelect={handleSelect} />
+			</div>
 			<div className="grid">
 				{items.map((item, index) => (
 					<EventCard key={index} item={item} />
